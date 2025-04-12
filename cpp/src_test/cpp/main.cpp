6e4fbfb7f42ba75dd2c220b79d5a7e6be6f6006e
imzhuli <imzhuli@vip.qq.com>
2025-04-12 11:17:29 +0800

    sync test with CoreX

--- cpp/src_test/cpp/main.cpp
+++ cpp/src_test/cpp/main.cpp
@@ -16,5 +16,24 @@ int main(int, char **) {
     IS.Release(Id);
     RuntimeAssert(!IS.Check(Id));
 
+    int i = 0;
+    int j = 0;
+
+    Reset(i, 1024);
+    cout << YN(i == 1024) << endl;
+
+    j = Steal(i, 777);
+    cout << YN(j == 1024) << endl;
+    cout << YN(i == 777) << endl;
+
+    j = Steal(i);
+    cout << YN(j == 777) << endl;
+    cout << YN(i == 0) << endl;
+
+    Reset(i, 666);
+    cout << YN(i == 666) << endl;
+    Reset(i);
+    cout << YN(i == 0) << endl;
+
     return 0;
 }
